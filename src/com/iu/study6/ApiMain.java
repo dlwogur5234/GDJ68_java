7443abdc7243f1872ed78f6a3147587976e76dcc
dlwogur5234 <dlwogur5234@naver.com>
2023-07-07 17:06:03 +0900

    0707

---
+++ src/com/iu/study6/ApiMain.java
@@ -0,0 +1,18 @@
+package com.iu.study6;
+
+public class ApiMain {
+
+	public static void main(String[] args) {
+		// °´Ã¼ »ý¼º
+		Object obj = new Object();
+		boolean check = obj.equals(null);
+		String str = obj.toString();
+		int h = obj.hashCode();
+		
+		String st = "hello";
+		
+		char ch =st.charAt();
+		System.out.println(ch);
+	}
+
+}
